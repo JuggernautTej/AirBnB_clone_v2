4f48a17790599bf5b82dee69d550cdf16747c31b
JuggernautTej <tiwatej@gmail.com>
2024-05-19 17:47:23 +0100

    added task 6 files

--- web_flask/6-number_odd_or_even.py
+++ web_flask/6-number_odd_or_even.py
@@ -45,5 +45,12 @@ def number_template(n):
     return render_template('5-number.html', n=n)
 
 
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def number_odd_or_even(n):
+    """The route for number odd or even"""
+    call = "even" if n % 2 == 0 else "odd"
+    return render_template('6-number_odd_or_even.html', n=n, call=call)
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
